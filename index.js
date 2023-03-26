eec6f6dda64f069dbac710b59b23c5351253ea49
Sterling <sterlingbarton81@gmail.com>
2023-03-25 20:58:57 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    const pTag = document.querySelector('#text');
+    pTag.textContent = 'This is really cool!';
+})
